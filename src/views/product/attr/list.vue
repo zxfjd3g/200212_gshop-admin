b1cffc74e55ddaf17c0b1529ac77e85b07f2cde7
zxfjd3g <258147149@qq.com>
2020-06-22 14:23:17 +0800

    6--实现: 属性添加&修改的界面

--- src/views/product/attr/list.vue
+++ src/views/product/attr/list.vue
@@ -4,32 +4,58 @@
       <CategorySelector @categoryChange="handleCategoryChange"/>
     </el-card>
     <el-card style="margin-top:20px;">
-      <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
+      <template v-if="isShowList">
+          <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
+        <el-table :data="attrs" border v-loading="loading">
+          <el-table-column label="序号" type="index" width="80" align="center"></el-table-column>
+          <el-table-column label="属性名称" prop="attrName" width="150"></el-table-column>
+          <el-table-column label="属性值列表">
+            <template slot-scope="{row, $index}">
+              <!-- 
+                {
+                  "id": 2051,
+                  "valueName": "支持存储卡",
+                  "attrId": 384
+                },
+              -->
+              <el-tag type="info" v-for="value in row.attrValueList" :key="value.id">
+                {{value.valueName}}
+              </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" width="150">
+            <template slot-scope="{row, $index}">
+              <hint-button title="修改属性" type="primary" icon="el-icon-edit" size="mini"></hint-button>
+              <HintButton title="删除属性" type="danger" icon="el-icon-delete" size="mini"></HintButton>
+            </template>
+          </el-table-column>
+        </el-table>
+      </template>
+      
+      <template v-else>
+        <el-form inline>
+          <el-form-item label="属性名">
+            <el-input type="text" placeholder="请输入属性名"></el-input>
+          </el-form-item>
+        </el-form>
 
-      <el-table :data="attrs" border v-loading="loading">
-        <el-table-column label="序号" type="index" width="80" align="center"></el-table-column>
-        <el-table-column label="属性名称" prop="attrName" width="150"></el-table-column>
-        <el-table-column label="属性值列表">
-          <template slot-scope="{row, $index}">
-            <!-- 
-              {
-                "id": 2051,
-                "valueName": "支持存储卡",
-                "attrId": 384
-              },
-            -->
-            <el-tag type="info" v-for="value in row.attrValueList" :key="value.id">
-              {{value.valueName}}
-            </el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column label="操作" width="150">
-          <template slot-scope="{row, $index}">
-            <hint-button title="修改属性" type="primary" icon="el-icon-edit" size="mini"></hint-button>
-            <HintButton title="删除属性" type="danger" icon="el-icon-delete" size="mini"></HintButton>
-          </template>
-        </el-table-column>
-      </el-table>
+        <el-button type="primary" icon="el-icon-plus">添加属性值</el-button>
+        <el-button @click="isShowList=true">取消</el-button>
+
+        <el-table border style="margin: 20px 0">
+          <el-table-column label="序号" type="index" width="80" align="center"></el-table-column>
+          <el-table-column label="属性值名称"></el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="{row, $index}">
+              <hint-button title="删除" type="danger" icon="el-icon-delete" size="mini"></hint-button>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <el-button type="primary">保存</el-button>
+        <el-button @click="isShowList=true">取消</el-button>
+      </template>
+
     </el-card>
   </div>
 </template>
@@ -46,6 +72,7 @@ export default {
       category3Id: null, // 三级分类ID
 
       attrs: [], // 属性列表
+      isShowList: false, // 是否显示属性列表界面, 如果是false显示添加&修改的界面
     }
   },
 
